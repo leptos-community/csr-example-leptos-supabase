1b8289a6c09656c7dc0358cba4df0c0a50a5d86a
Mahdi <mtzarrin@gmail.com>
2024-01-09 22:18:16 +0330

    change routes

--- src/app.rs
+++ src/app.rs
@@ -23,66 +23,72 @@ pub fn App() -> impl IntoView {
     view! {
         <Router>
             <Routes>
-                <Route
-                    path="/login"
-                    view=move || {
-                        if user.get_untracked().access_token.is_empty() {
-                            view! { <LogIn user=user set_user=set_user/> }
-                        } else {
-                            view! { <Redirect path="/"/> }
-                        }
-                    }
-                />
-
-                <Route path="/signup" view=SignUp/>
-
-                <Route
-                    path="/redirect"
-                    view=move || {
-                        let new_user = url_hash_to_user(use_location().hash.get());
-                        match new_user {
-                            Some(new_user) => {
-                                if user.get_untracked().uuid != new_user.uuid {
-                                    local_storage().clear().expect("Can't access to local storage");
-                                }
-                                set_user.set(new_user);
+                <Route path="/leptos_supabase_example" view=move|| view! { <Redirect path="/"/> }>
+                    <Route
+                        path="/login"
+                        view=move || {
+                            if user.get_untracked().access_token.is_empty() {
+                                view! { <LogIn user=user set_user=set_user/> }
+                            } else {
                                 view! { <Redirect path="/"/> }
                             }
-                            /// (Some(access_token), None) => _Never happens! This path is for login from google so both tokens always are provided
-                            _ => view! { <Redirect path="/login"/> },
                         }
-                    }
-                />
+                    />
 
-                <Route
-                    path="/"
-                    view=move || {
-                        if user.get_untracked().access_token.is_empty().not() {
-                            view! { <Home user=user set_user=set_user/> }
-                        } else {
-                            view! { <Redirect path="/login"/> }
+                    <Route path="/signup" view=SignUp/>
+
+                    <Route
+                        path="/redirect"
+                        view=move || {
+                            let new_user = url_hash_to_user(use_location().hash.get());
+                            match new_user {
+                                Some(new_user) => {
+                                    if user.get_untracked().uuid != new_user.uuid {
+                                        local_storage()
+                                            .clear()
+                                            .expect("Can't access to local storage");
+                                    }
+                                    set_user.set(new_user);
+                                    view! { <Redirect path="/"/> }
+                                }
+                                /// (Some(access_token), None) => _Never happens! This path is for login from google so both tokens always are provided
+                                _ => view! { <Redirect path="/login"/> },
+                            }
                         }
-                    }
-                />
+                    />
 
-                <Route
-                    path="signup/confirmation"
-                    view=move || {
-                        view! {
-                            <div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);font-size:22px;">
-                                "Please confirm your email from your inbox"
-                            </div>
+                    <Route
+                        path="/"
+                        view=move || {
+                            if user.get_untracked().access_token.is_empty().not() {
+                                view! { <Home user=user set_user=set_user/> }
+                            } else {
+                                view! { <Redirect path="/login"/> }
+                            }
                         }
-                    }
-                />
+                    />
 
-                <Route
-                    path="/*any"
-                    view=move || {
-                        view! { <div style="margin:auto; font-size:18;">"Page Not Found :("</div> }
-                    }
-                />
+                    <Route
+                        path="signup/confirmation"
+                        view=move || {
+                            view! {
+                                <div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);font-size:22px;">
+                                    "Please confirm your email from your inbox"
+                                </div>
+                            }
+                        }
+                    />
 
+                    <Route
+                        path="/*any"
+                        view=move || {
+                            view! {
+                                <div style="margin:auto; font-size:18;">"Page Not Found :("</div>
+                            }
+                        }
+                    />
+
+                </Route>
             </Routes>
             <div id="toast" class:show=show_toast>
                 {toast_text}
