211e07e568293525f5adc2c351d5c62730067e3b
Mahdi <mtzarrin@gmail.com>
2024-01-10 08:11:13 +0330

    add diversable action

--- src/app.rs
+++ src/app.rs
@@ -79,6 +79,7 @@ pub fn App() -> impl IntoView {
                 <Route
                     path="/*any"
                     view=move || {
+                        
                         view! { <div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);font-size:22px;">"Page Not Found :("</div> }
                     }
                 />
