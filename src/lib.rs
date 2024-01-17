0d1e307bcb0f6c73a970f4e0f53bfa486873e4b1
Mahdi <mtzarrin@gmail.com>
2024-01-18 00:03:35 +0330

    change from bin to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 use app::App;
-use leptos::*;
+use leptos::mount_to_body;
+use wasm_bindgen::prelude::wasm_bindgen;
 mod app;
 mod components;
 mod core;
 mod env;
 
-fn main() {
+#[wasm_bindgen(start)]
+pub fn start() {
     console_log::init_with_level(log::Level::Debug).unwrap_or_default();
     console_error_panic_hook::set_once();
     mount_to_body(App);
